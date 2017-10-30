ef1ff99136c42670243f2d5c2d9aa9ffb2a4d86e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-30 16:49:06 -0700

    implement RecipeSearchWrapper.search and Recipe.initialize methods

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,13 @@
 class Recipe
+  attr_reader :name, :image_url, :recipe_url, :source, :ingredients, :dietary_info, :diet_labels
 
-  def initialize
-    
+  def initialize(args)
+    @name = args[:name]
+    @image_url = args[:image_url]
+    @recipe_url = args[:recipe_url]
+    @source = args[:source]
+    @ingredients = args[:ingredients]
+    @dietary_info = args[:dietary_info]
+    @diet_labels = args[:diet_labels]
   end
 end
--- lib/recipe_search_wrapper.rb
+++ lib/recipe_search_wrapper.rb
@@ -1,8 +1,30 @@
 class RecipeSearchWrapper
-  ID = ENV["ID"]
-  KEY = ENV["KEY"]
-  BASE_URL = "https://api.edamam.com/search"
-  BUILD_URL = BASE_URL + "?app_id=#{ID}&app_key=#{KEY}"
+  ID = ENV["RECIPE_ID"]
+  KEY = ENV["RECIPE_KEY"]
+  BASE_URL = "https://api.edamam.com/"
 
-  
+  def self.search(query)
+    url = BASE_URL + "search?app_id=#{ID}&app_key=#{KEY}&q=#{query}"
+    response = HTTParty.get(url)['hits']
+    recipes = []
+    if response.empty?
+      return nil
+    else
+      response.each do |recipe|
+        recipe = recipe['recipe']
+        recipes << Recipe.new(
+          {
+            name: recipe['label'],
+            image_url: recipe['image'],
+            recipe_url: recipe['url'],
+            source: recipe['source'],
+            ingredients: recipe['ingredientLines'],
+            dietary_info: recipe['totalNutrients'],
+            diet_labels: recipe['healthLabels']
+          }
+        )
+      end
+    end
+    return recipes
+  end
 end
