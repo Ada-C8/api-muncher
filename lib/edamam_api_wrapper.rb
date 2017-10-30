c3ab6e170e08fe9e8b3fe830caee8029c6d4fdbd
Amy Lee <amy.yj.lee@gmail.com>
2017-10-30 16:38:02 -0700

    modified config/application.rb to ensure app includes custom libraries: :

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,5 +1,19 @@
 require 'httparty'
 
-class EdamamApiWrapper 
+class EdamamApiWrapper
+  BASE_URL = "https://api.edamam.com/search"
+  APP_ID = ENV["EDAMAM_ID"]
+  APP_KEY = ENV["EDAMAM_KEY"]
+
+  def self.search_recipe_results(search_term)
+    # if q
+      url = BASE_URL + "search?q=#{search_term}" + "&app_id= ${#{APP_ID}}" + "&app_key= ${#{APP_KEY}}"
+
+    # if r
+      url = url = BASE_URL + "search?r=#{search_term}" + "&app_id= ${#{APP_ID}}" + "&app_key= ${#{APP_KEY}}"
+
+
+
+  end
 
 end
