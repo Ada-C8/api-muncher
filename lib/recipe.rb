e6ce8730de40a2e2cab80ddde12bcb727e052440
averikitsch <aekitsch@gmail.com>
2017-10-30 16:39:19 -0700

    Recipe class

---
+++ lib/recipe.rb
@@ -0,0 +1,21 @@
+class Recipe
+  attr_reader :label, :uri, :image, :source, :url, :yield, :calories, :health_labels, :diet_labels, :ingredients, :dietary_info
+
+  def initialize(label, uri, options = {})
+
+    raise ArgumentError if name.nil? || name.empty? || id.nil? || id.empty?
+    @label = label
+    @uri = uri
+
+    @image = options[:image]
+    @source = options[:source]
+    @url = options[:url]
+    @yield = options[:yield]
+    @calories = options[:calories]
+    @health_labels = options[:labels]
+    @diet_labels = options[:diets]
+    @ingredients = options[:ingredients]
+    @dietary_info = options[:dietary]
+  end
+
+end
