5493fdb891eb8666a6b98111e220d6c624beee88
kee nam <nam.keeyeon@gmail.com>
2017-10-30 16:57:51 -0700

    Added dotenv-rails to Gemfile; generated Recipes controller

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -2,4 +2,14 @@ require "httparty"
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
+  APP_ID = ENV["APP_ID"]
+  APP_KEY = ENV["APP_KEY"]
+
+  def self.search_recipes(query)
+    puts "Querying recipe"
+    url = BASE_URL + "q=#{query}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
+    response = HTTParty.get(url)
+    
+    return response.parsed_response['hits']
+  end
 end
