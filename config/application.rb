032fd86337ab6a68280fa9e30fa41c0dd2692f73
Gale Harrington <gale.harrington19@gmail.com>
2017-10-30 16:27:14 -0700

    Add HTTParty gem, add to application.rb, and create edamam_api_wrapper.rb

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module ApiMuncher
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Automatically load all code from <rails_root>/lib
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
