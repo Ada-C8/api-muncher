4f3c1e20b4592939dd5b721dd87f7533e38c696d
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-30 16:05:02 -0700

    .env works! Base class definitions for wrapper & Recipe

--- lib/recipe_search_wrapper.rb
+++ lib/recipe_search_wrapper.rb
@@ -1,5 +1,8 @@
 class RecipeSearchWrapper
   ID = ENV["ID"]
   KEY = ENV["KEY"]
-  THIS = "that"
+  BASE_URL = "https://api.edamam.com/search"
+  BUILD_URL = BASE_URL + "?app_id=#{ID}&app_key=#{KEY}"
+
+  
 end
