71d40c2a87620fdadfe36bb49d4af6768a50ce8a
Amy Lee <amy.yj.lee@gmail.com>
2017-10-30 16:23:55 -0700

    added dotenv gem to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # root 'recipe#index' #not_implemented_yet
+
+  get ':recipe/', to: 'recipe#show', as: 'show_recipe'
+
+
 end
