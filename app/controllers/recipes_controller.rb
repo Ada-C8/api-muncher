eee1ce86b8cf4b3c253934d5b84a560a44530d47
averikitsch <aekitsch@gmail.com>
2017-10-31 16:55:13 -0700

    added sessions to keep track of searches

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,11 @@ require 'will_paginate/array'
 class RecipesController < ApplicationController
   def index
     @recipes = ApiMuncherWrapper.search_recipes(params[:search]).paginate(:page => params[:page], :per_page => 10)
+    if session[:recent_searches]
+      session[:recent_searches][params[:search]] = @recipes[0] ? @recipes[0].image : nil
+    else
+      session[:recent_searches] = {params[:search] => @recipes[0] ? @recipes[0].image : nil}
+    end
   end
 
   def show
