8f61396145cbef11a83f2eefb45f4480082296f7
kee nam <nam.keeyeon@gmail.com>
2017-10-31 16:18:13 -0700

    Edited view pages and cleaned up recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,18 @@
 class RecipesController < ApplicationController
 
+  def index
+    render layout: false
+  end
+
   def search
     @query = params[:query]
     @results = EdamamApiWrapper.search_recipes(@query)
     return @results
-    # check if the query is valid
-    # if query
-    #   @results = EdamamApiWrapper.search_recipes(query)
-    #   return @results
-    # end
   end
 
   def show
     @recipe_uri = params[:uri]
     @recipe = EdamamApiWrapper.show_recipe(@recipe_uri)
+    @recipe = @recipe[0]
   end
-
-  # private
-  #
-  # def recipe_params
-  #   params.require(:recipe).permit(:uri, :label, :image, :source, :url, :yield, :calories, :totalWeight, :ingredients, :totalNutrients, :totalDaily, :dietLabels, :healthLabels)
-  # end
 end
