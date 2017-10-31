8f3c0c4f7571bda5fee59aa2db65b4cffc971d44
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-31 09:43:15 -0700

    change RecipeSearchWrapper name to RecipeSearch

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   def index
     if params["search"]
-      @recipes = RecipeSearchWrapper.search(params["search"])      
+      @recipes = RecipeSearch.search(params["search"])      
     end
   end
 end
