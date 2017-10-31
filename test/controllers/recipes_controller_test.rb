09866d18596c985dc4120c7f7ebbd3ec2312c3af
averikitsch <aekitsch@gmail.com>
2017-10-31 16:55:35 -0700

    sessions test

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -11,6 +11,15 @@ describe RecipesController do
     end
   end
 
+  it "should save recent searches" do
+    VCR.use_cassette("index_action") do
+      get recipes_path, params: {search: "salad"}
+      get recipes_path, params: {search: "coffee"}
+      session[:recent_searches].must_be_instance_of Hash
+      session[:recent_searches].length.must_equal 2
+    end
+  end
+
   it "should get show" do
     VCR.use_cassette("show_action") do
       uri = "bcbbb3cd875fa61b5601231a709812a6"
