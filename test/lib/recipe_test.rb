19db8e92c85acf4fa33ff721525015d2265f898d
Gale Harrington <gale.harrington19@gmail.com>
2017-10-31 15:19:22 -0700

    Finish Recipe.rb tests

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -13,7 +13,24 @@ describe Recipe do
     my_recipe.source.must_equal "source"
   end
   it "requires 5 parameters" do
+    proc {
+      Recipe.new
+    }.must_raise ArgumentError
 
+    proc {
+      Recipe.new "label"
+    }.must_raise ArgumentError
+
+    proc {
+      Recipe.new "label", "url", "ingredients", "source"
+    }.must_raise ArgumentError
+  end
+  it "takes diet_labels and health_labels as optional params" do
+    my_recipe = Recipe.new "label", "url", "ingredients", "image", "source", {diet_labels: ["vegetarian", "vegan"], health_labels: ["nut-free"]}
+
+    my_recipe.wont_be_nil
+    my_recipe.diet_labels.must_be_instance_of Array
+    my_recipe.health_labels.must_be_instance_of Array
   end
 
 end
