5ad6a811dcbfa77228ec4235e5d76eb2ea83d1ae
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-31 12:11:23 -0700

    Recipe class initialize method.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,6 +2,12 @@ class Recipe
 
   def initialize(response)
     @title = response['recipe']['label']
+    @ingredients = response['recipe']['ingredientLines']
+    @calories = response['recipe']['calories']
+    @health_labels = response['recipe']['healthLabels']
+    @diet_labels = response['recipe']['dietLabels']
+    @image = response['recipe']['image']
+    @link = response['recipe']['url']
   end
 
 end
