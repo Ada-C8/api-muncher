347813ce895c237b6caa2e8d86a62d48d4bd7158
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-30 22:00:39 -0700

    set up .env, application.rb, and the start of the edamam wrapper

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'recipes/index'
+
+  get 'recipes/new'
+
+  get 'recipes/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+
+class EdamamApiWrapper
+  # Your code here!
+
+  BASE_URL = "https://api.edamam.com"
+  TOKEN = ENV["EDAMAM_TOKEN "]
+end
