98aab9c46e766557a03d307949d5ef4074d5421e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-31 10:37:02 -0700

    create test cassettes for search

--- test/lib/recipe_search_test.rb
+++ test/lib/recipe_search_test.rb
@@ -3,16 +3,20 @@ require "test_helper"
 describe RecipeSearch do
   describe 'self.search' do
     it 'returns an array of Recipes if search has matches' do
-      result = RecipeSearch.search('artichoke')
+      VCR.use_cassette('artichoke') do
+        result = RecipeSearch.search('artichoke')
 
-      result.must_be_kind_of Array
-      result.each {|r| r.must_be_kind_of Recipe}
+        result.must_be_kind_of Array
+        result.each {|r| r.must_be_kind_of Recipe}
+      end
     end
 
     it 'returns an empty array if search has no matches' do
-      result = RecipeSearch.search('NoRecipeWouldHaveThis')
+      VCR.use_cassette('NoRecipeWouldHaveThis') do
+        result = RecipeSearch.search('NoRecipeWouldHaveThis')
 
-      result.must_equal []
+        result.must_equal []
+      end
     end
   end
 end
