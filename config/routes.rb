6da639af2bd44fa5cc0b14d8b513d2b03e097f0b
kee nam <nam.keeyeon@gmail.com>
2017-10-30 18:52:12 -0700

    Added view for recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipes#index'
+
+  # get ':channel/new', to: 'chat#new', as: 'chat_new'
+  # post ':channel', to: 'chat#create', as: 'chat_create'
 end
