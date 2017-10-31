8816f602329c696879820a497c359c07397b9ae8
Gale Harrington <gale.harrington19@gmail.com>
2017-10-31 15:04:30 -0700

    Make more arguments required for Recipe objects

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -1,11 +1,11 @@
 class Recipe
   attr_reader :label, :url, :ingredients, :image, :source, :diet_labels, :health_labels
-  def initialize(options)
-    @label = options[:label]
-    @url = options[:url]
-    @ingredients = options[:ingredients]
-    @image = options[:image]
-    @source = options[:source]
+  def initialize(label, url, ingredients, image, source, options)
+    @label = label
+    @url = url
+    @ingredients = ingredients
+    @image = image
+    @source = source
     @diet_labels = options[:diet_labels]
     @health_labels = options[:health_labels]
   end
