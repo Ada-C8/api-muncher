a0f721729c55f93465a477eb1323eed93d94d9b7
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-31 13:15:15 -0700

    create BASE_URL, APP_ID, APP_KEY constants, create self.search and self.check_status methods

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,11 +1,29 @@
 require "HTTParty"
+class ApiError < StandardError
+end
 
 class EdamamApiWrapper
-  BASE_URL = "https://api.edamam.com/"
+  BASE_URL = "https://api.edamam.com/search?"
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
 
-  def self.search(term)
-    # use API to colelct results
+  def self.search(q)
+    url = BASE_URL + "q=#{q}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
+    response = HTTParty.get(url)
+  end
+
+
+  private
+  def self.create_recipe(api_params)
+    return Recipe.new(
+      # api_params["name"],
+      # api_params["id"]
+    )
+  end
+
+  def self.check_status(response)
+    unless response["ok"]
+      raise ApiError.new("API call to Edamam failed: #{response["error"]}")
+    end
   end
 end
