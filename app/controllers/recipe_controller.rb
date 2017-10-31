b7ab66f17a02a518cfb087af1f6b69d071eee0fe
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-31 16:09:35 -0700

    removed previous index method and moved to EdamamApiWrapper

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,11 +1,6 @@
 class RecipeController < ApplicationController
   def index
-    response = EdamamWrapper.search(params[:q])
 
-    response.hits.map do |result|
-      result[:recipe][:label]
-      result[:]
-    end
   end
 
   def show
