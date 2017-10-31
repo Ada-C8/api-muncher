ec78a1b34737f5bcade4391572fb42874e2dd53e
kee nam <nam.keeyeon@gmail.com>
2017-10-31 14:07:50 -0700

    Added edamam attribution

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,21 @@
 class RecipesController < ApplicationController
-  def index
-    # @recipes = EdamamApiWrapper.all_recipes
-    # @recipes = Recipe.all
-    # if params[:query]
-    #   @recipes = Recipe.search(params[:query]).order("created_at DESC")
-    # else
-    #   @recipes = Recipe.all.order("created_at DESC")
-    # end
-  end
+  # def index
+  #   # @recipes = EdamamApiWrapper.all_recipes
+  #   # @recipes = Recipe.all
+  #   # if params[:query]
+  #   #   @recipes = Recipe.search(params[:query]).order("created_at DESC")
+  #   # else
+  #   #   @recipes = Recipe.all.order("created_at DESC")
+  #   # end
+  # end
 
-  def new
+  def show
     # @recipe = params[:recipe]
   end
 
   def search
-    query = params[:query]
-    @results = EdamamApiWrapper.search_recipes(query)
+    @query = params[:query]
+    @results = EdamamApiWrapper.search_recipes(@query)
     return @results
     # if query
     #   @results = EdamamApiWrapper.search_recipes(query)
