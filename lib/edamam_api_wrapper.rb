d745086f5ecb77d3bf5549cbcd559a88b676b80a
Jessica Owens <vertige@gmail.com>
2017-10-31 16:32:27 -0700

    updated search method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -7,13 +7,22 @@ class EdamamApiWrapper
   APP_KEY = ENV["EDAMAM_KEY"]
   APP_ID = ENV["EDAMAM_ID"]
 
-  def self.search(query)
-    url = BASE_URL + "search?q=#{query}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
+  def self.search(query, hits)
+    url = BASE_URL + "search?q=#{query}" + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}" + "&more=1" + "$to=#{hits}"
 
     data = HTTParty.get(url)
     # ap data
     if data["hits"]
-      return data["hits"]
+      recipe_hits = data["hits"].map do |hit|
+        Recipe.new(
+          hit["recipe"]["label"],
+          hit["recipe"]["url"],
+          hit["recipe"]["image"],
+          hit["recipe"]["source"],
+          hit["recipe"]["ingredientLines"]
+        )
+      end
+      return recipe_hits
     else
       return []
     end
