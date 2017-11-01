183fedf2c7f885b62f6585015f38289ab9b58701
Shaunna <skwiens@gmail.com>
2017-11-01 11:19:21 -0700

    recipe class setup

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -8,12 +8,46 @@ describe Recipe do
     end
 
     it "must have a uri and a name" do
+      proc {
+        Recipe.new
+      }.must_raise ArgumentError
+      proc {
+        Recipe.new
+      }.must_raise ArgumentError
+    end
 
+    it "can accept optional parametrs" do
+
+
+    end
+  end
+
+  describe "attr_readers" do
+    it "can read a uri" do
+
+    end
+
+    it "can read a name" do
+
+    end
+
+    it "can read an image url" do
+
+    end
+
+    it "can read the ingredients" do
 
     end
 
 
+    it "can read the calories" do
 
+
+    end
+
+    it "can read the totalNutrients" do
+      
+    end
   end
 
 
