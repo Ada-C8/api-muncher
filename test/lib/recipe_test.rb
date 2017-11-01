297783a534ad05b51fff228c269bf7475a0579bb
Andrea Chen <andreachen.ac@gmail.com>
2017-10-31 21:08:54 -0700

    Add tests for Recipe#ingredients_text

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,10 +1,13 @@
 require 'test_helper'
 
 describe "Recipe" do
+  before do
+    @test_recipe = Recipe.new("label", "url", "uri", "image", [{"text" => "some ingredients", "weight" => 23}])
+  end
 
   describe "initialize" do
     it "should be instantiated with a label, url, uri, image and ingredients" do
-      Recipe.new("label", "url", "uri", "image", [{"text" => "some ingredients"}])
+      @test_recipe
     end
 
     it "should require a label, url, uri, image and ingredients " do
@@ -21,7 +24,7 @@ describe "Recipe" do
       url = "www.testurl.com"
       uri = "www.testuri.com"
       image = "www.testimage.jpg"
-      ingredients = [{"text" => "some ingredients"}]
+      ingredients = [{"text" => "some ingredients", "weight" => 23}]
 
       recipe = Recipe.new(label, url, uri, image, ingredients)
 
@@ -34,13 +37,24 @@ describe "Recipe" do
     end
   end
 
-
   describe "ingredients_text" do
+    it "should be able to be called" do
+      @test_recipe.must_respond_to :ingredients_text
+    end
 
+    it "should return an Array" do
+      @test_recipe.ingredients_text.must_be_instance_of Array
+    end
+
+    it "should only return the values for the 'text' key" do
+      @test_recipe.ingredients_text[0].must_equal "some ingredients"
+    end
   end
 
   describe "uri_id" do
+    it "text" do
 
+    end
   end
 
 end
