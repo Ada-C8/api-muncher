25141765a874c82c4bf9677dd0f694fa8a3f0fa3
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-01 11:00:41 -0700

    create pages controller???

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root 'main#index'
   get '/attributions', to: 'main#attribution', as: 'attributions'
 
-  # resources :recipes, only: [:index, :show]
-  get '/recipes', to: 'recipes#index', as: 'recipes'
+  resources :recipes, only: [:index] do
+    resources :pages, only: [:show]
+  end
   get '/recipes/*id', to: 'recipes#index', as: 'recipe_results'
   post '/recipes', to: 'recipes#search', as: 'search'
   get '/recipes/show/*id', to: 'recipes#show', as: 'recipe'
