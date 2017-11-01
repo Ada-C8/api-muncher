7c4d892e449239a889a65e8ff07642400a339456
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-31 22:42:19 -0700

    flesh out search method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ require 'edamam_api_wrapper'
 class RecipesController < ApplicationController
   def search
     response = EdamamApiWrapper.search(params[:q])
-    recipes = EdamamApiWrapper.get_results_from_response(response)
+    @recipes = EdamamApiWrapper.get_results_from_response(response)
+    render search_path
   end
 end
