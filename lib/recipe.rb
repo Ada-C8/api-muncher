134f86a3b9982cc44c08edab3678b3ec05a13e0d
Tanisha <tanishahampden@gmail.com>
2017-11-01 16:08:45 -0700

    update parameters and instance variables in constructor

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,9 +1,17 @@
 class Recipe
-  attr_reader :label
+  attr_reader :name, :id, :image_url, :url, :servings, :ingredients, :calories
 
-  def initialize(label)
-    raise ArgumentError if label == nil
-    @label = label
+  def initialize(name:, id:, url:, ingredients:, **options)
+    # raise ArgumentError if name == nil
+    @name = name
+    @id = id
+    @url = url
+    @ingredients = ingredients
 
+    @servings = options[:servings]
+    @calories = options[:calories]
+    @total_nutrients = options[:total_nutrients]
+    @health_labels = options[:health_labels]
+    @diet_labels = options[:diet_labels]
   end
 end
