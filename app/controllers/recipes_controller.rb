da1debf6116474ee39dfc6e5618f745d9f8498d0
Canaan West <canaan.west@gmail.com>
2017-11-01 15:59:02 -0700

    recipes controller tests for index and show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,16 @@ class RecipesController < ApplicationController
   def index
     @search = params[:search]
     @recipes = FoodApiWrapper.find_recipes(@search)
+    if @recipes.length == 0
+      flash[:result_text] = "No Results Found"
+    end
   end
 
   def show
     @uri = params[:uri]
     @recipe = FoodApiWrapper.show_recipe(@uri)
+    unless @recipe
+      render_404
+    end
   end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -3,34 +3,41 @@ require "test_helper"
 describe RecipesController do
   describe "index" do
     it "must have a main (search) page" do
-      get root_path
-      must_respond_with :success
+      VCR.use_cassette("index_test") do
+        get root_path
+        must_respond_with :success
+      end
     end
 
-    it "must get an index without a search term" do
-      get recipes_path
-      must_respond_with :success
+    it "must get an empty index without a search term or if a search has no results" do
+      VCR.use_cassette("index_test") do
+        get recipes_path, params: {term: ""}
+        must_respond_with :success
+      end
     end
 
     it "must get an index path when provided with a search term" do
-
-    end
-
-    it "must respond with not_found if a search term has no results" do
-
+      VCR.use_cassette("index_test") do
+        get recipes_path, params: {term: "chicken"}
+        must_respond_with :success
+      end
     end
   end
 
   describe "show" do
     it "must show a user the details for a recipe" do
-      get recipe_path(something)
-      must_respond_with :success
+      VCR.use_cassette("show_test") do
+        recipe = FoodApiWrapper.find_recipes("chicken").first
+        get recipe_path(recipe.name), params: {uri: recipe.uri}
+        must_respond_with :success
+      end
     end
 
     it "must respond with not_found if a recipe doesn't exist" do
-
+      VCR.use_cassette("show_test") do
+        get recipe_path("faulty test"), params: {uri: "baduri"}
+        must_respond_with :not_found
+      end
     end
-
   end
-
 end
