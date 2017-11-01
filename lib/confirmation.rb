1db0e53593d1c4019ae1e6f81bfab8b848082743
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-01 16:42:04 -0700

    changed the Confirmation module methods to be what I want them to be for use in the controller

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,13 +1,13 @@
 module Confirmation
-  def self.empty_search_term(search_term)
+  def empty(search_term)
     if search_term == ""
-      raise BlankSearchError.new("You entered a blank search term .... let's try that agine)")
+      return true
     end
   end
 
-  def self.contain_symbols(search_term)
+  def symbols(search_term)
     if !(search_term.match(/\A[[:alpha:][:blank:]]+\z/))
-      raise BadSearchTermError.new("Sorry, your search term cannot contain numbers or symbols: #{search_term}")
+      return true
     end # if
   end
 end # Confirmation
