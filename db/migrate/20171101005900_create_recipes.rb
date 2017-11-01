f7c50e6365b886be4ffb707af04f0f3a70309085
Test <test@Lindseys-iMac.local>
2017-10-31 18:08:25 -0700

    creating models

--- db/migrate/20171101005900_create_recipes.rb
+++ db/migrate/20171101005900_create_recipes.rb
@@ -1,7 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[5.1]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :image_url
+      t.string :creator
+       
       t.timestamps
     end
   end
