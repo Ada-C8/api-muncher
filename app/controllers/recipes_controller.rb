6793f4fd3061f066d53653790ec96d294e2d22dc
Canaan West <canaan.west@gmail.com>
2017-10-31 20:49:53 -0700

    removing create method from recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,10 +3,6 @@ class RecipesController < ApplicationController
   def search
   end
 
-  def create
-    #this is where i can make a call to "create" a new search??
-  end
-
   def index
     @search = params[:search]
     @recipes = FoodApiWrapper.find_recipes(@search)
