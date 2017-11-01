15d9a31113dece38d18ba0d6d195b239d76444bf
Tanisha <tanishahampden@gmail.com>
2017-11-01 11:20:43 -0700

    update apimuncherwrapper

--- lib/api_muncher_wrapper.rb
+++ lib/api_muncher_wrapper.rb
@@ -1,11 +1,31 @@
-require "httparty"
+require 'httparty'
 
 class ApiMuncherWrapper
-  BASE_URL = "https://api.edamam.com/"
+  BASE_URL = "https://api.edamam.com/search?q="
   EDAMAM_KEY = ENV["EDAMAM_KEY"]
   EDAMAM_ID = ENV["EDAMAM_ID"]
 
-  def self.search_recipes
+  class ApiError < StandardError
+  end
 
+  def self.list_recipes(search_term)
+    url = BASE_URL + search_term + "app_id=$" + EDAMAM_ID + "&app_key=$" + EDAMAM_KEY
+
+    data = HTTParty.get(url)
+
+    recipe_list = []
+
+    if data["hits"]
+      data["hits"].each do |result|
+        recipe_list << self.get_recipe(search_term)
+      end
+    end
+
+    return recipe_list
+  end
+
+  private
+  def self.get_recipe(search_term)
+    return Recipe.new(search_term)
   end
 end
