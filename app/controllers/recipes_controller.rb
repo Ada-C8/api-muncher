6c9587e96409a52eed5b5a00a0b0496df97a8280
Tanisha <tanishahampden@gmail.com>
2017-11-01 13:30:37 -0700

    update @recipes instance variable

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,4 @@
 class RecipesController < ApplicationController
   # TODO: pass in the proper parameter
-  @recipes = ApiMuncherWrapper.list_recipes("chicken")
+  @recipes = EdamamApiWrapper.list_recipes("chicken")
 end
