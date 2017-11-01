1ababf64c6dca6d831da8d8324cdc30771c5f310
Jan Edrozo <jedrozo@gmail.com>
2017-11-01 12:23:38 -0700

    Edamam_api_wrapper_test.rb (added test fr wrong app id and key)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,33 @@
 class RecipesController < ApplicationController
+  def index
+    @channels = EdamamApiWrapper.list_channels(search_string)
+  end
+
+  def search
+
+  end
+
+  # def create
+  #   result = SlackApiWrapper.send_msg(params["channel"], params["message"])
+  #
+  #   if result
+  #     flash[:success] = "Message Posted!"
+  #   else
+  #     flash[:error] = "Error!"
+  #   end
+  #
+  #   # redirect_to root_path
+  #
+  #   # message = params["message"]
+  #   # channel = params["channel"]
+  #   # response = SlackApiWrapper.send_msg(channel, message)
+  #   #
+  #   # if response
+  #   #   flash[:success] = "Message Posted!"
+  #   # else
+  #   #   flash[:error] = "Error!"
+  #   # end
+  #
+  #   redirect_to chat_new_path(params["channel"])
+  # end
 end
--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -8,6 +8,8 @@ describe EdamamApiWrapper do
 
   describe "self#list_recipes" do
     it "Can list a group of channels" do
+    end
+    it "Can list a group of channels" do
       VCR.use_cassette("recipes") do
         recipes = EdamamApiWrapper.list_recipes(@search_string)
         recipes.must_be_instance_of Array
@@ -21,6 +23,14 @@ describe EdamamApiWrapper do
     it "Will return [] for a broken request" do
       VCR.use_cassette("recipes") do
         channels = EdamamApiWrapper.list_recipes(@bogus_search_str)
+        channels.must_be_instance_of Array
+        channels.must_equal []
+      end
+    end
+
+    it "Will return [] for invalid app_id and app_key" do
+      VCR.use_cassette("recipes") do
+        channels = EdamamApiWrapper.list_recipes(@search_string, "wrong_app_id", "wrong_app_key")
         channels.must_be_instance_of Array
         channels.must_equal []
       end
