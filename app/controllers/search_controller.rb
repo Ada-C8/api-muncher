8d921ccdfd74b7e2cf1bce61fff830bcccac4137
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-11-01 14:48:02 -0700

    Functioning routes that are cleaned up and make actions much clearer.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,10 +5,6 @@ class SearchController < ApplicationController
 
   end
 
-  def new
-  end
-
-
   def create
     @keywords = params['ingredients']
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'search#new'
+  root 'search#index'
 
-  post '/search/:ingredients/:page', to: 'search#create', as: 'search_results'
+  get '/search', to: 'search#create', as: 'search_results'
 
   get '/search/:title', to: 'search#show_recipe', as: 'show_recipe'
 
