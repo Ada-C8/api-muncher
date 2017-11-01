a2144663fc0ffba63d57ee6cdfda70adf99584d8
averikitsch <aekitsch@gmail.com>
2017-11-01 12:06:14 -0700

    migration and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/create'
-
-  get 'users/logout'
 
   root 'recipes#root'
+
+  get '/logout', to: "users#logout"
+
+  get "/auth/:provider/callback", to: "users#create"
 
   get '/recipes/', to: 'recipes#index', as: 'recipes'
 
   get 'recipes/:uri', to: 'recipes#show', as: 'recipe'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
