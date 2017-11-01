4da0af4ac6c5461fde89fd9a82923662448efbf6
enigmagnetic <lauren.cardella@gmail.com>
2017-11-01 15:37:17 -0700

    Add search form to search/root view. Clean up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root 'recipes#search'
 
-  get 'recipes/search'
+  get 'recipes/search', to: 'recipes#search', as: 'recipe_search'
 
   get 'recipes/index'
 
   get 'recipes/show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'recipe#search'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
 end
