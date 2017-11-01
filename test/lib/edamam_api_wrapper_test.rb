8cf7b193ad5aec94938aca7cbad88168c843c979
Jan Edrozo <jedrozo@gmail.com>
2017-11-01 11:04:14 -0700

    Added basic self#list_recipes for 2 basic tests (mirrored from SlackAPI)

---
+++ test/lib/edamam_api_wrapper_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+describe EdamamApiWrapper do
+  before do
+    @search_string = "chicken"
+    @bogus_search_str = "1234567%$!"
+  end
+
+  describe "self#list_recipes" do
+    it "Can list a group of channels" do
+      VCR.use_cassette("recipes") do
+        recipes = EdamamApiWrapper.list_recipes(@search_string)
+        recipes.must_be_instance_of Array
+
+        recipes.each do |recipe|
+          recipe.must_be_instance_of Recipe
+        end
+      end
+    end
+
+    it "Will return [] for a broken request" do
+      VCR.use_cassette("recipes") do
+        channels = EdamamApiWrapper.list_recipes(@bogus_search_str)
+        channels.must_be_instance_of Array
+        channels.must_equal []
+      end
+    end
+  end
+end
