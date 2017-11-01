e787e984ba5562d5e7bd40e4cbd2369a08ee6c9b
averikitsch <aekitsch@gmail.com>
2017-11-01 14:27:08 -0700

    oauth implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def create
+    @auth_hash = request.env['omniauth.auth']
+    @user = User.find_by(uid: @auth_hash['uid'],provider: @auth_hash['provider'])
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back #{@user.name}!"
+    else
+        @user = User.new(uid: @auth_hash['uid'],
+        provider: @auth_hash['provider'], name: @auth_hash['info']['name'], name: @auth_hash['info']['name'], email:@auth_hash['info']['email'])
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "Welcome #{@user.name}!"
+      else
+        flash[:failure] = "Unable to log in!"
+      end
+    end
+    redirect_to root_path
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :uid, presence: true, uniqueness: true
+  validates :provider, presence: true
 end
