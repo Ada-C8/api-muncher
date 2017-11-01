6cb7715b7526e8adf016ea6b6408f32252a0b0a1
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-01 13:42:20 -0700

    get rid of extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#main'
-  resources :recipes
+  
+  get '/recipes', to: 'recipes#index', as: 'recipes'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
 
   get '/search', to: 'recipes#search', as: 'search'
 end
