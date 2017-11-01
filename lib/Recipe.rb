0957559dd245f21293ecec3c5e3310b38d88aae1
Sara Frandsen <sara.frandsen@gmail.com>
2017-10-31 23:09:18 -0700

    organize initialize to include optional args

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -1,15 +1,16 @@
 class Recipe
   attr_reader :name, :image, :source, :url, :servings, :diet, :health, :ingredients, :calories
 
-  def initialize(name, image, source, url, servings, diet, health, ingredients, calories )
+  def initialize(name, image, source, url, options={})
     @name = name
     @image = image
     @source = source
     @url = url
-    @servings = servings
-    @diet = diet
-    @health = health
-    @ingredients = ingredients
-    @calories = calories
+
+    @servings = options[:servings]
+    @diet = options[:diet]
+    @health = options[:health]
+    @ingredients = options[:ingredients]
+    @calories = options[:calories]
   end
 end
