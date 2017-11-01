1314df52fd63df66b28d072687eaa39bf821a0d2
enigmagnetic <lauren.cardella@gmail.com>
2017-11-01 11:12:31 -0700

    Add copyright and attribution to footer. Create root route to search page. Create recipes controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'recipes#search'
+
+  get 'recipes/search'
+
+  get 'recipes/index'
+
+  get 'recipes/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'recipe#search'
 end
