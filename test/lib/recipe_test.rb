c7d03fe764feaf1e449722fb231a6a41a5625c8b
Andrea Chen <andreachen.ac@gmail.com>
2017-10-31 20:24:56 -0700

    Add first test for Recipe class

---
+++ test/lib/recipe_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+describe "Recipe" do
+
+  describe "initialize" do
+    it "Can be instantiated with a label, url, uri, image and ingredients" do
+      Recipe.new("label", "url", "uri", "image", "ingredients")
+    end
+  end
+
+
+  describe "ingredients_text" do
+
+  end
+
+  describe "uri_id" do
+
+  end
+
+end
