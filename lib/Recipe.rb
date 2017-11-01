6a9f4c0f4810e9ec55380a1f5bce46affa3beed1
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-01 11:29:44 -0700

    change what is required in Recipe

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -1,16 +1,16 @@
 class Recipe
   attr_reader :name, :image, :source, :url, :servings, :diet, :health, :ingredients, :calories
 
-  def initialize(name, image, source, url, options={})
+  def initialize(name, image, source, url, ingredients, options={})
     @name = name
     @image = image
     @source = source
     @url = url
-
+    @ingredients = ingredients
+    #optional
     @servings = options[:servings]
     @diet = options[:diet]
     @health = options[:health]
-    @ingredients = options[:ingredients]
     @calories = options[:calories]
   end
 end
