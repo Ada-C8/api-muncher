b3ef37150e93ce66652e5ec5d135f6c3c81481e4
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-01 12:13:52 -0700

    configured VCR

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,21 @@ Minitest::Reporters.use!(
   Minitest.backtrace_filter
 )
 
+VCR.configure do |config|
+  # make cassette files
+  config.cassette_library_dir = 'test/cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes, # create new episode if nothing match
+    :match_requests_on => [:method, :uri, :body] # if it matches existing cassette, replay episode
+  }
+  config.filter_sensitive_data("<EDAMAM_KEY>") do
+    ENV['EDAMAM_KEY'] #anytime it sees this variable, replace with string <SLACK_TOKEN>
+  end
+  config.filter_sensitive_data("<EDAMAM_ID>") do
+    ENV['EDAMAM_ID'] #anytime it sees this variable, replace with string <SLACK_TOKEN>
+  end
+end
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
