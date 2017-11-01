44094ee3a1dbb74de8bc179c0c62f8d1eea1eeef
Shaunna <skwiens@gmail.com>
2017-11-01 10:57:14 -0700

    layout for recipe class

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,12 +1,12 @@
 class Recipe
 
-  attr_reader :uri, :label, :image, :ingredients, :dietaryInfo, :url, :calories, :totalNutrients
+  attr_reader :uri, :name, :image, :ingredients, :dietaryInfo, :url, :calories, :totalNutrients
 
   def initialize(uri, label, options = {})
     raise ArgumentError if label == nil || label == "" || uri == nil || uri == ""
 
     @uri = URI.escape(uri)
-    @label = label
+    @name = label
     @image = options[:image]
     @ingredients = options[:ingredients]
     @url = options[:url]
---
+++ test/lib/recipe_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+describe Recipe do
+  describe 'create recipe' do
+    it "can create a recipe" do
+
+
+    end
+
+    it "must have a uri and a name" do
+
+
+    end
+
+
+
+  end
+
+
+
+end
