57e0be855f09e8900abae62a5fedae416b473f58
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-01 10:07:58 -0700

    Created method to send HTTP request for recipe json

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,8 +1,32 @@
 require "httparty"
+require "awesome_print"
 
 class EdamamApiWrapper
   # Your code here!
   BASE_URL = "https://api.edamam.com/"
   APP_KEY = ENV["EDAMAM_KEY"]
   APP_ID = ENV["EDAMAM_ID"]
+
+  def self.search_recipes(query)
+    url = BASE_URL + "search?app_key=#{APP_KEY}" + "&app_id=#{APP_ID}" + "&q=#{query}" # 1 = true, 0 = false
+
+    data = HTTParty.get(url)
+
+    if data["hits"].count > 0
+      recipes = data["hits"].map do |recipe_hash|
+        Recipe.new(
+          recipe_hash["recipe"]["label"],
+          recipe_hash["recipe"]["url"],
+          recipe_hash["recipe"]["ingredientLines"],
+          dietary_info: recipe_hash["recipe"]["dietLabels"],
+          health_info: recipe_hash["recipe"]["healthLabels"],
+          image: recipe_hash["recipe"]["image"],
+          source: recipe_hash["recipe"]["source"],
+          yield:recipe_hash["recipe"]["yield"])
+      end
+      return recipes
+    else
+      return[]
+    end
+  end
 end
