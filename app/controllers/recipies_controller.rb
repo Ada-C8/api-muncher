0b63ed2123b492e2f219c707cfe7872f543b0b84
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-02 13:35:25 -0700

    added

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -11,6 +11,7 @@ class RecipiesController < ApplicationController
       results = EdamamApiWrapper.get_recipies(params[:search_term])
       @search_term = params[:search_term]
       @recipies = results.paginate(:page => params[:page], :per_page => 10)
+      return @recipies
     rescue EdamamApiWrapper::NoResultsError
       flash[:status] = :failure
       flash[:message] = "Sorry, no recipies match that serch term."
