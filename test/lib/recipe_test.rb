2e8731672ed188ae33865ca8acef22a3d29b2b65
Tanisha <tanishahampden@gmail.com>
2017-11-02 10:30:16 -0700

    add recipe class tests

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 describe Recipe do
+  it "must initialize name, id, url, and ingredients properly" do
+    recipe = Recipe.new(name: "Name", id: "ID", url: "www.url.com", ingredients: ["sugar", "spinach"])
+    recipe.name.must_equal "Name"
+    recipe.id.must_equal "ID"
+    recipe.url.must_equal "www.url.com"
+    recipe.ingredients.must_equal ["sugar", "spinach"]
+  end
 
+  it "Cannot be initialized with less than 4 parameters" do
+    proc {
+      Recipe.new
+    }.must_raise ArgumentError
 
-
+    proc {
+      Recipe.new "Name"
+    }.must_raise ArgumentError
+  end
 end
