f5b6b21879f0c1be223f149aa9262f9b655d25bf
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-02 10:04:31 -0700

    update create_recipe to use BASE_URI instead of uri_encode

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -5,6 +5,7 @@ end
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
+  BASE_URI = "r=http://www.edamam.com/ontologies/edamam.owl%23recipe_"
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
 
@@ -26,27 +27,15 @@ class EdamamApiWrapper
     return recipes_array
   end
 
-  def self.create_recipe(uri)
-    encoded_uri = URI.encode(uri)
-    url = BASE_URL + uri + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
-    response = HTTParty.get(url)
-    # Recipe.new(
-    #   result[0]["uri"], # uri
-    #   result[0]["label"], # name
-    #   result[0]["image"], # image
-    #   result[0]["source"], # original source
-    #   result[0]["url"], # link to original
-    #   result[0]["ingredientLines"], # ingredients as an array
-    #   {
-    #     servings: result[0]["yield"], # servings
-    #     # nutritional info
-    #     diet: result[0]["dietLabels"], # low-fat, etc
-    #     health: result[0]["healthLabels"], # vegetarian, etc
-    #     calories: result[0]["calories"] # calories
-    #   }
-    # )
-    return response
-  end
+
+    def self.create_recipe(uri)
+      uri = uri.split("_")[-1]
+      url = BASE_URL + "r=" BASE_URI + uri + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
+      response = HTTParty.get(url)
+      return url
+    end
+
+    # "https://api.edamam.com/search?r=http://www.edamam.com/ontologies/edamam.owl%23recipe_032bd3c98053c6157bf5bb42ef653df5&app_id=7f1f7a18&app_key=d54b2b30925e1965c49d43881d33df43"
 
   private
   def self.check_status(response)
@@ -58,19 +47,19 @@ class EdamamApiWrapper
   def self.get_recipe(result)
     result_params = result["recipe"]
     return Recipe.new(
-      result_params["uri"],
-      result_params["label"], # name
-      result_params["image"], # image
-      result_params["source"], # original source
-      result_params["url"], # link to original
-      result_params["ingredientLines"], # ingredients as an array
-      {
-        servings: result_params["yield"], # servings
-        # nutritional info
-        diet: result_params["dietLabels"], # low-fat, etc
-        health: result_params["healthLabels"], # vegetarian, etc
-        calories: result_params["calories"] # calories
-      }
+      result_params["uri"], # uri
+      # result_params["label"], # name
+      # result_params["image"], # image
+      # result_params["source"], # original source
+      # result_params["url"], # link to original
+      # result_params["ingredientLines"], # ingredients as an array
+      # {
+      #   servings: result_params["yield"], # servings
+      #   # nutritional info
+      #   diet: result_params["dietLabels"], # low-fat, etc
+      #   health: result_params["healthLabels"], # vegetarian, etc
+      #   calories: result_params["calories"] # calories
+      # }
     )
   end
 end
