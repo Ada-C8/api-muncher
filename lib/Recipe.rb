9de176656ec0544417a2f1896d20ce3c64f1accf
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-02 10:16:32 -0700

    uncomment args for Recipe

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -1,17 +1,17 @@
 class Recipe
   attr_reader :uri, :name, :image, :source, :url, :servings, :diet, :health, :ingredients, :calories
 
-  def initialize(uri)#, name, image, source, url, ingredients, options={})
-    @uri = uri.split("_")[-1]
-    # @name = name
-    # @image = image
-    # @source = source
-    # @url = url
-    # @ingredients = ingredients
-    # #optional
-    # @servings = options[:servings]
-    # @diet = options[:diet]
-    # @health = options[:health]
-    # @calories = options[:calories]
+  def initialize(uri, name, image, source, url, ingredients, options={})
+    @uri = uri
+    @name = name
+    @image = image
+    @source = source
+    @url = url
+    @ingredients = ingredients
+    #optional
+    @servings = options[:servings]
+    @diet = options[:diet]
+    @health = options[:health]
+    @calories = options[:calories]
   end
 end
