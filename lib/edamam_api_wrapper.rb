60216a564fce1ad8ba9abe6721e12fa0884c1d7e
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-02 10:16:52 -0700

    create_recipe now returns correct url

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -5,7 +5,7 @@ end
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
-  BASE_URI = "r=http://www.edamam.com/ontologies/edamam.owl%23recipe_"
+  BASE_URI = "http://www.edamam.com/ontologies/edamam.owl%23recipe_"
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
 
@@ -27,15 +27,13 @@ class EdamamApiWrapper
     return recipes_array
   end
 
-
+    # to get the full url incl. uri
     def self.create_recipe(uri)
       uri = uri.split("_")[-1]
-      url = BASE_URL + "r=" BASE_URI + uri + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
+      url = BASE_URL + "r=" + BASE_URI + uri + "&app_id=#{APP_ID}" + "&app_key=#{APP_KEY}"
       response = HTTParty.get(url)
       return url
     end
-
-    # "https://api.edamam.com/search?r=http://www.edamam.com/ontologies/edamam.owl%23recipe_032bd3c98053c6157bf5bb42ef653df5&app_id=7f1f7a18&app_key=d54b2b30925e1965c49d43881d33df43"
 
   private
   def self.check_status(response)
@@ -48,18 +46,18 @@ class EdamamApiWrapper
     result_params = result["recipe"]
     return Recipe.new(
       result_params["uri"], # uri
-      # result_params["label"], # name
-      # result_params["image"], # image
-      # result_params["source"], # original source
-      # result_params["url"], # link to original
-      # result_params["ingredientLines"], # ingredients as an array
-      # {
-      #   servings: result_params["yield"], # servings
-      #   # nutritional info
-      #   diet: result_params["dietLabels"], # low-fat, etc
-      #   health: result_params["healthLabels"], # vegetarian, etc
-      #   calories: result_params["calories"] # calories
-      # }
+      result_params["label"], # name
+      result_params["image"], # image
+      result_params["source"], # original source
+      result_params["url"], # link to original
+      result_params["ingredientLines"], # ingredients as an array
+      {
+        servings: result_params["yield"], # servings
+        # nutritional info
+        diet: result_params["dietLabels"], # low-fat, etc
+        health: result_params["healthLabels"], # vegetarian, etc
+        calories: result_params["calories"] # calories
+      }
     )
   end
 end
