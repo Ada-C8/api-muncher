7a37b8ed2ff101c5d4aea41d215d73815f099387
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-02 10:04:45 -0700

    no change

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,11 +2,11 @@ require 'edamam_api_wrapper'
 class RecipesController < ApplicationController
 
   def index
-    # @recipes = EdamamApiWrapper.search(params[:q])
+    @recipes = EdamamApiWrapper.search(:q)
   end
 
   def show
-    @recipe = EdamamApiWrapper.create_recipe(params[:recipe_uri])
+    @recipe = params[:recipe]
   end
 
   def search
@@ -14,7 +14,4 @@ class RecipesController < ApplicationController
     @recipes = EdamamApiWrapper.get_results_from_response(response)
     render search_path
   end
-
-  private
-
 end
