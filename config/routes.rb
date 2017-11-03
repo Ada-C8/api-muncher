43832c860565d06ff9eaa98402690c92549047dc
Tanisha <tanishahampden@gmail.com>
2017-11-03 14:25:32 -0700

    add alias for show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
   # resources :recipes, only: [:index, :show]
-  get 'recipes', to: 'recipes#show'
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
 end
