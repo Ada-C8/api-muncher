85f05dc04f0cb379fd44ddcb348903d537d07b36
Angela Wilson <wilsonange@gmail.com>
2017-11-02 20:30:20 -0700

    write Recipe class tracks required parameters test and all code passes.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,18 +1,18 @@
 class Recipe
-  attr_reader :name, :original_url, :image, :ingredients, :dietLabels
+  attr_reader :name, :original_url, :image, :ingredientsLine, :dietLabels
 
   # the hard parameters are the required bits to create an instance. The options hash is for data that is nice to have an not required
-  def initialize( name, original_url, ingredients, dietLabels options = {} )
-    raise ArgumentError if name == nil || name == "" ||
+  def initialize( name, original_url, ingredientsLine, dietLabels, options = {} )
+    raise ArgumentError if name == nil || name == "" || original_url == nil || original_url == "" || ingredientsLine == nil || ingredientsLine == "" || dietLabels == nil || dietLabels == ""
 
-    @name = name
-    @original_url = original_url
-    @ingredients = ingredients
-    @dietLabels = dietLabels
-
-    # Options hash stuff
-
-    @image = options[:image]
+    @name = name # name is a string
+    @original_url = original_url # original_url is a string
+    @ingredientsLine = ingredientsLine # ingredientsLine is array of strings
+    @dietLabels = dietLabels # dietLabels is an array of strings
+    #
+    # # Options hash stuff
+    #
+    # @image = options[:image] # image is a url in string format
 
 
   end # end initialize
--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,24 +1,34 @@
 require 'test_helper'
 
 describe "Recipe Class" do
+  before do
+    @recipe = Recipe.new("name", "original_url", "ingredients", "dietLabels")
+  end
   describe "initialize" do
     it "Can be instanciated with name, original_url, ingredients, dietLabels" do
-      Recipe.new("name", "original_url", "ingredients", "dietLabels").must_be_instance_of Recipe
+      @recipe.must_be_instance_of Recipe
     end
 
-    it "Raises ArgumentError if name is either nil or empty string" do
+    it "Raises ArgumentError if name, original_url, ingredients, dietLabels is either nil or empty string" do
       proc {
         Recipe.new()
       }.must_raise ArgumentError
 
       proc {
-        Recipe.new("", "")
+        Recipe.new("")
       }.must_raise ArgumentError
+
+      proc {
+        Recipe.new("name")
+      }
 
     end
 
     it "Tracks required parameters: name, original_url, ingredients, dietLabels" do
-
+      @recipe.name.must_equal "name"
+      @recipe.original_url.must_equal "original_url"
+      @recipe.ingredientsLine.must_equal "ingredients"
+      @recipe.dietLabels.must_equal "dietLabels"
     end
 
 
