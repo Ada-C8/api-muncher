3458002d73f6f0fa9a9d8d0ddb4d8b98b253da45
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-03 11:59:49 -0700

    change @uri from uri to uri.split

--- lib/Recipe.rb
+++ lib/Recipe.rb
@@ -2,7 +2,7 @@ class Recipe
   attr_reader :uri, :name, :image, :source, :url, :servings, :diet, :health, :ingredients, :calories
 
   def initialize(uri, name, image, source, url, ingredients, options={})
-    @uri = uri
+    @uri = uri.split("_")[-1]
     @name = name
     @image = image
     @source = source
