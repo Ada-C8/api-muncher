d9a40cf00a2c38776a3e8e9b3f284db0d7545321
Julia <julia.meier@gmail.com>
2017-11-03 14:58:17 -0700

    additional refactoring

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,25 +4,19 @@ require 'pry'
 
 class RecipesController < ApplicationController
 
-
   #THIS IS THE SEARCH PAGE WITH QUERY BAR
   def index
-
   end
 
   #THIS DISPLAYS THE RESULTS OF WHAT WE FOUND WHEN WE DID THE SEARCH
   def search
-    @keywords = params['keywords']
-    if @keywords.nil?
-      redirect_to root_path
-    else
-      @number_of_recipes = EdamamApiWrapper.find_number_of_recipes(@keywords)
-      # if params['from']
-      #   @recipes = EdamamApiWrapper.list_recipes(@keywords, 0, 9)
-      # else
+      @keywords = params['keywords']
+      if @keywords.nil?
+        redirect_to root_path
+      else
+        @number_of_recipes = EdamamApiWrapper.find_number_of_recipes(@keywords)
         @recipes = EdamamApiWrapper.list_recipes(@keywords, 0, 9)
-      # end
-    end
+      end
   end
 
   def search_by_page
@@ -32,15 +26,6 @@ class RecipesController < ApplicationController
       #add flash message maybe
     else
       @number_of_recipes = EdamamApiWrapper.find_number_of_recipes(@keywords)
-
-      if @number_of_recipes > 100
-        @greater_than_100 = true
-        @number_of_recipes_on_last_page = 10
-      else
-        @greater_than_100 = false
-        @number_of_recipes_on_last_page = @number_of_recipes%10
-      end
-
       from = params[:from]
       to = params[:to]
       @recipes = EdamamApiWrapper.list_recipes(@keywords, from, to)
@@ -51,10 +36,5 @@ class RecipesController < ApplicationController
   def show
     @recipe = EdamamApiWrapper.show_recipe(params['id'])
   end
-
-  private
-
-  # def find_number_of_recipes_on_last_page
-
 
 end
