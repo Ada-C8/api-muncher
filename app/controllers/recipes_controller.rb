8be7aa10d615b5315246ca697445adf5ad0a6a68
enigmagnetic <lauren.cardella@gmail.com>
2017-11-03 01:41:09 -0700

    Edit actions in recipes controller to remove search and build out index. Edit application and recipe index views. Search not returning any results.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,17 @@
 class RecipesController < ApplicationController
-  def search
-  end
 
   def index
+    if params[:query]
+      @recipes = EdamamApiWrapper.search(params[:query])
+    else
+      redirect_to root_path
+    end
   end
 
   def show
+  end
+
+  def task_params
+    params.require(:recipe).permit(:query)
   end
 end
