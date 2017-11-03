32fbc9bd696f74b0fdacc8575ac00ff9303182cb
Jessica Owens <vertige@gmail.com>
2017-11-02 17:47:30 -0700

    Added foundation and changed file names

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'httparty'
 gem 'awesome_print'
+gem 'foundation-rails', '6.4.1.2'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'recipes/:recipe', to: 'recipe#show', as: 'show_recipe'
-  root 'recipe#search_form'
+  root 'recipe#home'
   get 'recipes/', to: 'recipe#list_search', as: 'recipes'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
