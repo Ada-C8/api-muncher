09a3c264065f2a14cccbb8c3ce8c56bf77e8b86b
Shaunna <skwiens@gmail.com>
2017-11-02 21:12:37 -0700

    tests for success

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,55 +2,49 @@ require "test_helper"
 
 describe RecipesController do
   describe "index" do
-    it "can get a list of recipes" do
+    it "responds with success with no search term" do
       VCR.use_cassette("index_action") do
         get root_path()
         must_respond_with :success
-
-        @recipes.must_be_instance_of Array
-        @recipes.each do |recipe|
-          recipe.must_be_instance_of Recipe
-        end
       end
     end
 
-    it "defualts a list of recipes if a search is not done before" do
-      # VCR.use_cassette("index_action") do
-      #
-      #
-      # end
-    end
-
     it "successfully loads page with many recipes" do
-
-
+      VCR.use_cassette("index_action") do
+        get root_path("water")
+        must_respond_with :success
+      end
     end
 
-    it "loads page with no recipes" do
-
-
-    end
-
-    it "loads page with a request with no recipes" do
-
-
+    it "successfully loads for a bogus search" do
+      VCR.use_cassette("index_action") do
+        get root_path("2.4ft")
+        must_respond_with :success
+      end
     end
   end
 
   describe "show" do
-    it "finds a recipe and loads the page" do
-      # VCR.use_cassette("show_action") do
-      #   get recipe_path( )
-      #   @recipe.must_be_instance_of Recipe
-      #   must_respond_with :success
-      # end
+
+    let(:uri) {URI.encode("http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2")}
+
+    it "loads the show page" do
+      VCR.use_cassette("show_action") do
+        recipe = EdamamApiWrapper.item_search(uri)
+        get recipe_path(recipe.name, uri: recipe.uri )
+        must_respond_with :success
+      end
     end
 
     it "renders a 404 if the recipe is not found" do
-      # VCR.use_cassette("show_action") do
-      #   get recipe_path("Bogus")
-      #   must_respond_with :not_found
-      # end
+      VCR.use_cassette("show_action") do
+        get recipe_path("Bogus")
+        must_respond_with :not_found
+      end
     end
+
+    it ""
+
+
   end
 end
