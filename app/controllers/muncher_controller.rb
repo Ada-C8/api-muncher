079e2b8a7b260f04042d615954590ef3042b9df4
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-03 15:45:11 -0700

    deleted muncher controller

--- app/controllers/muncher_controller.rb
+++
@@ -1,6 +0,0 @@
-class MuncherController < ApplicationController
-  # QUESTION: Why isn't the controller name plural ?
-
-  
-
-end
--- test/controllers/muncher_controller_test.rb
+++
@@ -1,5 +0,0 @@
-require "test_helper"
-
-describe MuncherController do
-  
-end # MuncherController
