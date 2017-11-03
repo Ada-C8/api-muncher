9af654cdb39cf15aa9967a31ae37aaf37d349123
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-03 13:35:14 -0700

    refactor search to use params instead of IDs, more RESTful routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,24 +2,17 @@ class RecipesController < ApplicationController
   PAGE_ITEMS = 12
 
   def index
-    if params["id"]
-      @query = params["id"]
-      @page = (params["page"] || 1).to_i
+    if params['search']
+      @query = params['search']
+      @page = (params['page'] || 1).to_i
 
-      recipes = RecipeSearch.search(params["id"], (@page - 1), PAGE_ITEMS, true)
+      recipes = RecipeSearch.search(params['search'], (@page - 1), PAGE_ITEMS, true)
       build_pages(recipes)
     end
   end
 
   def show
     @recipe = RecipeSearch.find(params["id"])
-  end
-
-  def search
-    if params["search"]
-      @query = params["search"]
-      redirect_to recipe_results_path(@query)
-    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get '/attributions', to: 'main#attribution', as: 'attributions'
 
   resources :recipes, only: [:index]
-  get '/recipes/:id', to: 'recipes#index', as: 'recipe_results'
-  post '/recipes', to: 'recipes#search', as: 'search'
+  # get '/recipes/:id', to: 'recipes#index', as: 'recipe_results'
+  # post '/recipes', to: 'recipes#search', as: 'search'
   get '/recipe/*id', to: 'recipes#show', as: 'recipe'
 
 end
