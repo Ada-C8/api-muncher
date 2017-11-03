9b89a06e1ac0bdc8fbb0ed9922c0759b9556b809
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-03 15:01:14 -0700

    Add case when search results empty

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,14 @@
 class RecipesController < ApplicationController
 
   def root
-    
+
   end
 
   def search
     @recipes = EdamamApiWrapper.search_recipes(params[:query])
   end
 
-  def create
+  def show
+    @recipe = Recipe.find_by()
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'recipes#root'
   # get 'search/:query', to: 'recipes#search', as: 'search'
   get 'recipes', to: 'recipes#search', as: 'recipes'
-  post 'recipe', to: 'recipes#create'
+  # post 'recipe', to: 'recipes#create'
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
 end
