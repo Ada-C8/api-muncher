5357d03e5da102d4b2e540707c7a1640c152fde7
Stef <seedsofthesesame@gmail.com>
2017-11-04 16:04:42 -0700

    Controller updated with proper methods being called.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,13 +5,13 @@ class RecipesController < ApplicationController
 
   def index
 
-    @recipes = EdamamApiWrapper.search(params[:search_word]).paginate(page: params[:page], per_page: 10)
+    @recipes = EdamamApiWrapper.list_recipes(params[:search_word]).will_paginate(page: params[:page], per_page: 10)
 
   end
 
   def show
 
-    @recipe = EdamamApiWrapper.item_search(params[:recipe_uri])
+    @recipe = EdamamApiWrapper.show_recipe(params[:recipe_uri])
 
   end
 end
