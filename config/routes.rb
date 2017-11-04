7eb336fd15dfa4fd015bf1a538babdccda00ceb0
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-04 15:51:47 -0700

    Added basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'recipes#search'
+
+  get 'recipes/', to: 'recipes#index', as: 'recipes'
+
+  get 'recipes/:ingredient_query/recipe/:id', to: 'recipes#show', as: 'recipe'
 end
