b33fa3063949f35bdbc0260a732fcdb161e535df
Tanisha <tanishahampden@gmail.com>
2017-11-04 11:50:54 -0700

    add code to load lib files

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module ApiMuncher
 
     # Automatically load all code from <rails_root>/lib
     config.autoload_paths << Rails.root.join('lib')
+
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
