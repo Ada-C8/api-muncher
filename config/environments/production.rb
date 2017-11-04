47b3ef27d4b7c490565ccd54fc0a95c6b25bae1d
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-11-03 21:13:50 -0700

    troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
+  config.enable_dependency_loading = true
+
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
