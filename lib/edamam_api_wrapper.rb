669b34e926a1c686d6324750de30c2a4b07c8390
Stef <seedsofthesesame@gmail.com>
2017-11-03 20:12:22 -0700

    Updated self method, still recieving error.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -7,6 +7,20 @@ class EdamamApiWrapper
   KEY = ENV["EDAMAM_KEY"]
   ID = ENV["Edamam_App_ID"]
 
+  def self.show_recipe(search_word)
+
+    url = BASE_URL + "r=" + (URI(search_word))
+
+    data = HTTParty.get(url)
+
+    Recipe.new data[0]["uri"],
+    data[0]["label"],
+    data[0]["image"],
+    data[0]["ingredientLines"],
+    data[0]["calories"],
+    data[0]["url"]
+  end
+
   # Method to list recipes.
   # Created method using Postman to have a better comprehension of how the api works, the information is found, as well as, working with the displayed data (hashes within hashes), utlizing the key value pair to pull only the information necessary/required for my app.  For example, I initially called 'hits', 'recipes'.  I changed it to 'hits' after viewing the JSON results and found that it is in fact called hits and I am taking the 'hits' key to pull the other key 'recipe' and with that calling for the value pair of necessary information that I need for my eventual output.
 
@@ -31,22 +45,9 @@ class EdamamApiWrapper
     else
       return []
     end
-
-    # Method to show recipes.
-
-    def self.show_recipe(search_word)
-
-      url = BASE_URL + "r=" + (URI(search))
-
-      data = HTTParty.get(url)
-
-      Recipe.new data[0]["uri"],
-      data[0]["label"],
-      data[0]["image"],
-      data[0]["ingredientLines"],
-      data[0]["calories"],
-      data[0]["url"]
-
-    end
   end
+
+  # Method to show recipes.
+
+
 end
