197584540f495ff4e55b01676ae5940d68c71ce1
Jan Edrozo <jedrozo@gmail.com>
2017-11-04 00:45:08 -0700

    Updated RecipeController tests (still need to fix show test and id)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,9 @@ class RecipesController < ApplicationController
   def index
     @search_string = params[:search]
 
-    if @search_string == nil || @search_string == " "
+    if @search_string.blank?
+      flash[:error] = "Please enter a search term."
+      redirect_to root_path
       return @recipes = nil
     else
       return @recipes = EdamamApiWrapper.list_recipes(@search_string).paginate(page: params[:page], per_page: 10)
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -6,13 +6,62 @@ describe RecipesController do
       get root_path
       must_respond_with :success
     end
+
+    it "stays on home page if search term is an empty string" do
+      VCR.use_cassette("recipes") do
+        get root_path(search: "")
+        must_respond_with :success
+      end
+
+      VCR.use_cassette("recipes") do
+        get root_path(search: "          ")
+        must_respond_with :success
+      end
+    end
   end
 
   describe "#index" do
 
+    it "successfully loads search results for any search term" do
+
+      VCR.use_cassette("recipes") do
+        get recipes_path(search: "chicken")
+        must_respond_with :success
+      end
+
+      VCR.use_cassette("recipes") do
+        get recipes_path(search: "%")
+        must_respond_with :success
+      end
+    end
+
+    it "redirects to root_path if search term is blank" do
+      VCR.use_cassette("recipes") do
+        get recipes_path(search: "  ")
+        flash.keys.must_include "error"
+        must_redirect_to root_path
+      end
+    end
   end
 
   describe "#show" do
+    #HELP
+    it "shows a recipe page" do
+      VCR.use_cassette("recipes") do
+        id = "http://www.edamam.com/ontologies/edamam.owl%23recipe_7bf4a371c6884d809682a72808da7dc2"
+        get recipe_path(id)
+        must_respond_with :success
+      end
+    end
+
+    it "renders 404 if the recipe page is not found" do
+      VCR.use_cassette("recipes") do
+        invalid_id = "7bf4a371c6884d809682a72808da7dc2"
+        get recipe_path(invalid_id)
+
+        must_respond_with :missing
+      end
+    end
 
   end
 end
