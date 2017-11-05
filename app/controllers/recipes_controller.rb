35b64782b7e9f5fc80f70222c9c9c7c2f8b61fad
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-11-04 23:23:12 -0700

    added styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,21 @@
+require 'will_paginate/array'
+
 class RecipesController < ApplicationController
   def home
+
   end
 
   def index
     #returns a list of searched criteria
     query = params["search"]
-    # @recipes = EdamamApiWrapper.search(query).paginate(page: params[:page], per_page: 10)
-    @recipes = EdamamApiWrapper.search(query)
+
+    if params["search"] == ""
+      flash[:status] = :error
+      flash[:result_text] = "Please enter a food you'd like to eat"
+      redirect_to root_path
+    else
+      @recipes = EdamamApiWrapper.search(query).paginate(page: params[:page], per_page: 10)
+    end
   end
 
   def show
