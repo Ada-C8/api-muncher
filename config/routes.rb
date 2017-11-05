d2bbb21db80282890cf4b7aded1dd2c6b32953a3
enigmagnetic <lauren.cardella@gmail.com>
2017-11-05 14:23:41 -0800

    Clean up unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root 'recipes#search'
 
-  # get 'recipes/search', to: 'recipes#search', as: 'recipe_search'
   get 'recipes/show', to: 'recipes#show', as: 'recipe'
 
   get 'recipes/index', to: 'recipes#index', as: 'recipes'
-
-  # get 'recipes/search?:query', to: 'recipes#index'
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
 end
