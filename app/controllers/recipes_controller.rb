3e3b1908e3c74ebac23ed9ac8851fa43e86e3d75
Shaunna <skwiens@gmail.com>
2017-11-04 19:44:59 -0700

    private method success_status controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,22 +8,25 @@ class RecipesController < ApplicationController
     @recipes = EdamamApiWrapper.search("Thanksgiving").paginate(page: params[:page], per_page: 12)
 
     if @recipes
-      flash[:status] = :success
-      flash[:result_text] = "We're so happy to have you here at Munchers! Check out our current seasonal favorites below or search for something to munch on"
+      message = "We're so happy to have you here at Munchers! Check out our current seasonal favorites below or search for something to munch on"
+      success_status(message)
+      # flash[:result_text] = "We're so happy to have you here at Munchers! Check out our current seasonal favorites below or search for something to munch on"
     end
-
-
   end
 
   def index
     @recipes = EdamamApiWrapper.search(params[:term]).paginate(page: params[:page], per_page: 12)
 
     if @recipes == []
-      flash.now[:status] = :error
-      flash.now[:result_text] = "Sorry! We couldn't find anything delicious flavours matching #{params[:term]} Please try a different flavor!"
+      # flash.now[:status] = :error
+      message = "Sorry! We couldn't find anything delicious flavours matching #{params[:term]} Please try a different flavor!"
+      success_status(message)
+      # flash.now[:result_text] = "Sorry! We couldn't find anything delicious flavours matching #{params[:term]} Please try a different flavor!"
     else
-      flash.now[:status] = :success
-      flash.now[:result_text] = "Results found for #{params[:term]}"
+      # flash.now[:status] = :success
+      message = "Results found for #{params[:term]}"
+      success_status(message)
+      # flash.now[:result_text] = "Results found for #{params[:term]}"
     end
   end
 
@@ -31,10 +34,19 @@ class RecipesController < ApplicationController
     @recipe = EdamamApiWrapper.item_search(params[:uri])
 
     if @recipe
-      flash.now[:status] = :success
-      flash.now[:result_text] = "#{@recipe.name} found!"
+      message = "#{@recipe.name} found!"
+      success_status(message)
+      # flash.now[:status] = :success
+      # flash.now[:result_text] = "#{@recipe.name} found!"
     end
 
     render_404 unless @recipe
+  end
+
+  private
+
+  def success_status(message)
+    flash[:status] = :success
+    flash[:result_text] = message
   end
 end
