185a8e39574fbec6de573904f1e5698151fe5002
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-04 22:17:25 -0700

    updates

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -3,32 +3,31 @@ require 'test_helper'
 describe EdamamApiWrapper do
     describe "list_recipes" do
 
+      it "Can list a group of recipes" do
 
-    it "Can list a group of recipes" do
+        VCR.use_cassette("recipes") do
+          recipes = EdamamApiWrapper.list_recipes("apple")
+          recipes.must_be_instance_of Array
 
-      VCR.use_cassette("recipes") do
-        recipes = EdamamApiWrapper.list_recipes("apple")
-        recipes.must_be_instance_of Array
-
-        recipes.each do |recipe|
-          recipe.must_be_instance_of Recipe
+          recipes.each do |recipe|
+            recipe.must_be_instance_of Recipe
+          end
         end
       end
-    end
 
 
-    it "Will return [] for a broken request" do
-      VCR.use_cassette("recipes") do
-        recipes = EdamamApiWrapper.list_recipes(" ")
-        recipes.must_be_instance_of Array
-        recipes.must_equal []
+      it "Will return [] for a broken request" do
+        VCR.use_cassette("recipes") do
+          recipes = EdamamApiWrapper.list_recipes(" ")
+          recipes.must_be_instance_of Array
+          recipes.must_equal []
+        end
       end
-    end
-
 
   end
 
   describe "show_recipe" do
+
     it "Can show a requested recipe" do
       VCR.use_cassette("recipes") do
         uri = "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
@@ -36,6 +35,6 @@ describe EdamamApiWrapper do
         recipe.must_be_instance_of Recipe
       end
     end
-  end
 
+  end
 end
