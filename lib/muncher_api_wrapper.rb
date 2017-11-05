b881d2cc4a1d441403849096c99a328c702fd622
Diane Lenertz <lenertzd@grinnell.edu>
2017-11-05 14:27:50 -0800

    remove error raising in ApiWrapper

--- lib/muncher_api_wrapper.rb
+++ lib/muncher_api_wrapper.rb
@@ -9,16 +9,16 @@ class MuncherApiWrapper
   class ApiError < StandardError
   end
 
-  def self.search(search_term, from = 0, id=ID)
-    url = BASE_URL + "search?q=" + search_term + "&app_id=#{id}" + "&app_key=#{KEY}" + "&from=#{from}"
+  def self.search(search_term, from = 0)
+    url = BASE_URL + "search?q=" + search_term + "&app_id=#{ID}" + "&app_key=#{KEY}" + "&from=#{from}"
     puts "about to send request for recipes"
     data = HTTParty.get(url)
     puts "got response"
     puts " status: #{data.code}: #{data.message}"
     puts "parsed_response is #{data.parsed_response}"
-    unless data["ok"]
-      raise ApiError.new("Call to list recipes failed")
-    end
+    # unless data["ok"]
+    #   raise ApiError.new("Call to list recipes failed")
+    # end
     recipes_list = []
     if data["hits"]
       data["hits"].each do |recipe_data|
