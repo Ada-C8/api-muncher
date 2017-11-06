d6f726dcc521fa6f65edf175cd7268aca0386654
Sara Frandsen <sara.frandsen@gmail.com>
2017-11-05 19:20:11 -0800

    rename Recipe.rb to recipe.rb

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ApiMuncher
   class Application < Rails::Application
-   config.generators do |g|
-     # Force new test files to be generated in the minitest-spec style
-     g.test_framework :minitest, spec: true
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
 
-     # Always use .js files, never .coffee
-     g.javascript_engine :js
-   end
+      # Always use .js files, never .coffee
+      g.javascript_engine :js
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
@@ -22,7 +22,7 @@ module ApiMuncher
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
-    
+
     config.eager_load_paths << Rails.root.join('lib')
   end
 end
