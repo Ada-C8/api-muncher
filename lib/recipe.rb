e7c0496379e875ebfffca60f8a02c857411042bf
Tanisha <tanishahampden@gmail.com>
2017-11-06 07:39:32 -0800

    add recipe attributes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,14 +1,14 @@
 class Recipe
   attr_reader :name, :uri, :image, :url, :servings, :ingredients, :calories
 
-  def initialize(name:, uri:, url:, ingredients:, image:, **options)
+  def initialize(name:, uri:, image:, **options)
     # raise ArgumentError if name == nil
     @name = name
     @uri = uri #URI.encode(uri)
     @url = url
     @ingredients = ingredients
+    @ingredients_lines = ingredient_lines
     @image = image
-
     @servings = options[:servings]
     @calories = options[:calories]
     @total_nutrients = options[:total_nutrients]
