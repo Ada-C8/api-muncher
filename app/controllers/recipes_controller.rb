6efbd747f88627a818ea1fddb34acf4abb0db719
Angela Wilson <wilsonange@gmail.com>
2017-11-05 17:18:51 -0800

    add pagination functionality, success (exclamation point since the symbol is not permitted

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,9 +6,12 @@ class RecipesController < ApplicationController
 
   def index
     @query = params[:search]
-    
+
     if params[:search]
-      @recipes = ApiWrapper.list_recipes(params[:search])
+      # source for pagination https://hackhands.com/pagination-rails-will_paginate-gem/
+
+      # to paginate on an array source, https://stackoverflow.com/questions/4352895/ruby-on-rails-will-paginate-an-array
+      @recipes = ApiWrapper.list_recipes(params[:search]).paginate(:page => params[:page], :per_page => 10)
     else
       flash[:status] = :failure
       flash[:message] = "Invalid search term, please search for something else"
