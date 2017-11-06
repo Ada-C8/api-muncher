cfac0d89944795aeb1eccaaced1fa39eb61867b9
kee nam <nam.keeyeon@gmail.com>
2017-11-05 22:22:27 -0800

    Added ruby version to Gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,16 +92,17 @@ Rails.application.configure do
   config.enable_dependency_loading = true
 
 
-#IMAGES
-# Disable Rails's static asset server (Apache or nginx will already do this)
-config.serve_static_assets = false
+  #IMAGES
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
-# Compress JavaScripts and CSS
-config.assets.compress = true
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
-# Don't fallback to assets pipeline if a precompiled asset is missed
-config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
-# Generate digests for assets URLs
-config.assets.digest = true
+  # Generate digests for assets URLs
+  config.assets.digest = true
+  
 end
