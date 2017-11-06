2d0d6e0008b6b484308fae4b16a0028b4ca7ed41
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-06 03:09:33 -0800

    Wrote tests for recipe show page

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -23,6 +23,23 @@ describe RecipesController do
         must_respond_with :success
       end
     end
-
   end
+
+  describe "show" do
+    it "can display the show page for a valid recipe from a query" do
+      VCR.use_cassette("ok_uri_recipe_show")do
+        uri = "c5a89acb06b3f1cb35da47c3a070bb43"
+        get recipe_path(uri)
+        must_respond_with :success
+      end
+    end
+
+    it "redirects to a 404 page if the recipe uri is invalid" do
+      VCR.use_cassette("bad_uri_recipe_show") do
+        get recipe_path("bad_uri")
+        must_respond_with :not_found
+      end
+    end
+  end
+  
 end
