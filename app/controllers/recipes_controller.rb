4fcfc287d26ba510383d98f13c9942032d2f7013
Tanisha <tanishahampden@gmail.com>
2017-11-06 22:15:33 -0800

    modify index method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,11 +3,14 @@ require_dependency '../../lib/recipe'
 
 class RecipesController < ApplicationController
   def index
-    @recipes = EdamamApiWrapper.list_recipes(params[:query] || "chocolate")
+    @recipes = EdamamApiWrapper.list_recipes(params[:query])
   end
 
   def show
     # TODO: currently does not hit this controller action
     @recipe = EdamamApiWrapper.show_recipe(params[:uri])
+    unless @recipe
+      head :not_found
+    end
   end
 end
