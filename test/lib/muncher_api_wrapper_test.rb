97597e6283f91cca51f047430ebaaded7ea4bce6
Diane Lenertz <lenertzd@grinnell.edu>
2017-11-07 09:31:00 -0800

    final commit

--- test/lib/muncher_api_wrapper_test.rb
+++ test/lib/muncher_api_wrapper_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 describe MuncherApiWrapper do
-  # it "can get a list of recipes" do
-  #   VCR.use_cassette("searches") do
-  #     result = MuncherApiWrapper.search("term")
-  #   end
-  # end
-  #
-  # it "returns an array of recipes" do
-  #   VCR.use_cassette("searches") do
-  #     result = MuncherApiWrapper.search("term")
-  #     result.must_be_kind_of Array
-  #     result.length.must_be :>, 0
-  #     result.each do |recipe|
-  #       recipe.must_be_kind_of Recipe
-  #     end
-  #   end
-  # end
+  it "can get a list of recipes" do
+    VCR.use_cassette("searches") do
+      result = MuncherApiWrapper.search("term")
+    end
+  end
 
-  # it "does something when key is bad" do
+  it "returns an array of recipes" do
+    VCR.use_cassette("searches") do
+      result = MuncherApiWrapper.search("term")
+      result.must_be_kind_of Array
+      result.length.must_be :>, 0
+      result.each do |recipe|
+        recipe.must_be_kind_of Recipe
+      end
+    end
+  end
+
+  # it "does something when uri is bad" do
   #   VCR.use_cassette("searches") do
   #     proc {
   #     result = MuncherApiWrapper.search("term", 0, "bogus_key")
