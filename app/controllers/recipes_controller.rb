86fdfa8e51b933cfb0b7185e8eb0184d3281f0b7
Stef <seedsofthesesame@gmail.com>
2017-11-07 15:22:28 -0800

    Errors/Flash messages working.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,8 @@ class RecipesController < ApplicationController
     @recipes = EdamamApiWrapper.list_recipes(params[:search_word]).paginate(:page => params[:page], :per_page => 9)
 
     if @recipes.empty?
+      flash[:status] = :error
+      flash[:result] = "Please try again, no results found for #{params[:search_word]}"
       redirect_to root_path
     end
   end
