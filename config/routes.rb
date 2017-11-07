5222adae949523eab84f4d3d9d77b6c526b79376
Tanisha <tanishahampden@gmail.com>
2017-11-06 22:08:58 -0800

    add main#index rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'recipes#index'
+  root 'main#index'
   # resources :recipes, only: [:index, :show]
   get 'recipes/:uri', to: 'recipes#show', as: 'recipe'
+  get 'recipes', to: 'recipes#index'
 end
