ea5d9fa30884100e952b75233b2dc9aa7c5567ba
Mariana <lmarianarp@gmail.com>
2017-11-07 16:52:08 -0800

    Fix test for one recipe and show flash messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,8 +6,8 @@ class RecipesController < ApplicationController
   def index
     @recipes = MuncherApiWrapper.find_recipes(params[:word], params[:page])
     if @recipes == []
-      flash.now[:status] =  :not_found
-      flash.now[:message] = "No hay recetas con la palabra #{params[:word]}"
+      flash[:status] =  :not_found
+      flash[:message] = "No hay recetas con la palabra #{params[:word]}"
       redirect_back fallback_location: root_path
     else
       flash[:status] = :success
