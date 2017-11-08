1cad55dd16e5af41c53e0574280fea3766125c04
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-11-08 10:31:23 -0800

    Add tests for wrapper

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -8,9 +8,14 @@ class EdamamApiWrapper
   CRED_URL = "&app_id=#{ENV["APP_ID"]}&app_key=#{ENV["APP_KEY"]}"
   NUMBER_URL = "&from=0&to=900"
 
+  class ApiError < StandardError; end
+
   def self.search_recipes(search)
     url = BASE_URL + SEARCH_URL + "#{search}" + CRED_URL + NUMBER_URL
     response = HTTParty.get(url)
+
+    check_status(response)
+
     recipes = []
     if response["hits"]
       response["hits"].each do |recipe_data|
@@ -25,13 +30,24 @@ class EdamamApiWrapper
   def self.show_recipe(id)
     url = BASE_URL + SHOW_URL + "#{id}" + CRED_URL
     response = HTTParty.get(url)
+
+    check_status(response)
+
     if response.present?
       recipe = create_recipe(response[0])
       return recipe
+    else
+      raise ApiError.new("Recipe does not exist")
     end
   end
 
   private
+
+  def self.check_status(response)
+    unless response.code == 200
+      raise ApiError.new("API call to Edamam failed: #{response["error"]}")
+    end
+  end
 
   def self.create_recipe(api_params)
     return Recipe.new(
