bbb8e72e194d6cc0bbcefc019fba98962558a160
VirtualLindsey <tapircode@gmail.com>
2017-11-07 22:02:02 -0800

    adding VCR from branch i forgot to merge

--- test/controllers/recipie_controller_test.rb
+++ test/controllers/recipie_controller_test.rb
@@ -2,49 +2,62 @@ require 'test_helper'
 
 describe RecipesController do
   describe "index" do
-    it "must returns with success" do
-      get root_path
-      must_respond_with :success
-    end
+    it "can return list of matching recipes" do
+     VCR.use_cassette("index_action") do
+       get root_path
+       must_respond_with :success
+     end
+   end
   end
 
   describe "results" do
     it "must returns with success" do
-      params = {
-        q: "chicken",
-        from: 0
-      }
-      get  get_recipes_path, params: params
-      must_respond_with :success
+      VCR.use_cassette("results_action") do
+        params = {
+          q: "chicken",
+          from: 0
+        }
+        get  get_recipes_path, params: params
+        must_respond_with :success
+      end
     end
 
     it "returns blank array when query not found" do
-      params = {
-        q: "lasjdlkfasjdf",
-        from: 0
-      }
-      get  get_recipes_path, params: params
-      must_respond_with :success
+      VCR.use_cassette("results_action") do
+        params = {
+          q: "lasjdlkfasjdf",
+          from: 0
+        }
+        get  get_recipes_path, params: params
+        must_respond_with :success
+      end
     end
 
     it "message is dispalyed if query is blank" do
-      params = {
-        from: 0
-      }
-      get  get_recipes_path, params: params
-      must_respond_with :success
+      VCR.use_cassette("results_action") do
+        params = {
+          from: 0
+        }
+        get  get_recipes_path, params: params
+        must_respond_with :success
+      end
     end
   end
 
   describe "show" do
     it "must returns with success" do
-      get show_recipe_path("recipe_5264be9d66d1276ce4c5f5f97260235d")
-      must_respond_with :success
+      VCR.use_cassette("show_action") do
+        get show_recipe_path("recipe_5264be9d66d1276ce4c5f5f97260235d")
+        must_respond_with :success
+      end
     end
 
     it "must returns with success for bad query" do
-      get  show_recipe_path("garbage")
-      must_respond_with :success
+      VCR.use_cassette("show_action") do
+        get  show_recipe_path("garbage")
+        must_respond_with :success
+      end
     end
   end
+
 end
